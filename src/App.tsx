50f184e574445b2e4a25f4160329616cc8b875e2
HADEER-ESS <hadeeressam21@gmail.com>
2026-05-09 18:53:24 +0300

    diplay the created cusome components on App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
 import './App.css'
-import CourseGoal from './components/CourseGoal'
 import Header from './components/Header'
 import image from "./assets/hero.png"
+import { useState } from 'react'
+import CourseGoalList from './components/CourseGoalList'
+import GoalForm from './components/GoalForm'
+
+export type CourseGoal = {
+  id: number;
+  title: string;
+  description : string;
+}
 
 function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([])
+
+  function handleAddGoal(title: string, desc: string){
+    const goalItem : CourseGoal = {
+      id: Math.random(),
+      title: title,
+      description: desc
+    }
+    setGoals(prevState => {
+      return [...prevState, goalItem]
+    })
+  }
+
+  function handleDeleteGoal(id : number){
+    setGoals(prevState => prevState.filter(goal => goal.id !== id))
+  }
 
   return (
-    <main>
-      <Header image={{src: image, alt: "A List of goals"}}>
-        <h1>My Course Goals</h1>
-      </Header>
-      <CourseGoal
-        title='Learn React'
-        onDelete={()=>{}}
-      >
-        <p>Build a project using Reactjs and TypeScript</p>
-      </CourseGoal>
+    <main className="app-shell">
+      <div className="app-card">
+        <Header image={{src: image, alt: "A List of goals"}}>
+          <h1>My Course Goals</h1>
+        </Header>
+        <GoalForm onAddGoal={handleAddGoal}/>
+        <CourseGoalList onDelete={handleDeleteGoal} goals={goals} />
+      </div>
     </main>
   )
 }
